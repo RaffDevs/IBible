be58f01cf5113b72ed0f588656fde1f4776c8e8c
RaffDevs <rveronezdeveloper@gmail.com>
2023-04-13 17:35:25 -0300

    Initial Commit

---
+++ IBible/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBible
+//
+//  Created by Rafael Veronez Dias on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
